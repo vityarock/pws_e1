20a21676a3c12c92676c9f7b273d8741a8a1e623
Виктор <57987552+vityarock@users.noreply.github.com>
2022-01-09 10:09:56 +0300

    Update test_gallows.py

--- test_gallows.py
+++ test_gallows.py
@@ -2,17 +2,14 @@ import pytest
 from gallows import *
 import builtins
 
-
-
-
 builtins.foo = ['_', '_', '_', '_', '_', '_', '_', '_']
 target = 'coverage'
 answer = 'e'
 
+
 def test_letter_check():
     test_result = letter_check(target, answer)
+    print(test_result)
     assert test_result == True, "тест не пройден"
-
-
 
 test_letter_check()
