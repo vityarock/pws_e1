ce647448d97f67a34ce2792665731af03fe4c3a8
Виктор <57987552+vityarock@users.noreply.github.com>
2022-01-09 09:53:57 +0300

    Update test_gallows.py

--- test_gallows.py
+++ test_gallows.py
@@ -9,7 +9,6 @@ builtins.foo = ['_', '_', '_', '_', '_', '_', '_', '_']
 target = 'coverage'
 answer = 'e'
 
-@pytest.mark.xfail()
 def test_letter_check():
     test_result = letter_check(target, answer)
     assert test_result == True, "тест не пройден"
